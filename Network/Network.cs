715cf1cba2b16db1eda081f96e0ac3bd98516d09
Must_nu73@itstep.edu.az <Must_nu73@itstep.edu.az>
2021-07-10 18:29:19 +0400

    Add project files.

---
+++ Network/Network.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Network
+{
+  
+
+    static class Mail
+    {
+        private static SmtpClient SmtpClient { get; set; }
+        private static string SenderAddress { get; } = "mail";
+        private static string SenderPassword { get; } = "pass";
+        static Mail()
+        {
+            SmtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(SenderAddress, SenderPassword),
+                EnableSsl = true
+            };
+        }
+
+        public static void SendMail(in string recipient, in string subject, in string body)
+        {
+            SmtpClient.Send(SenderAddress, recipient, subject, body);
+        }
+    }
+
+}
